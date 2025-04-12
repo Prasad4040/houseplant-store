76679e49b4415abeaf664dd2258b5b2592b795e3
Prasad4040 <prasad.patil221@vit.edu>
2025-04-12 19:18:46 +0530

    Implemented Shopping Cart Page functionality

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import ProductListingPage from './components/ProductListingPage';
+import ShoppingCartPage from './components/ShoppingCartPage'; // We'll create this later
+import Header from './components/Header'; // Import the Header
 
 function App() {
   return (
     <div>
+      <Header /> {/* Include the Header component */}
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/products" element={<ProductListingPage />} /> {/* This route should exist */}
+        <Route path="/products" element={<ProductListingPage />} />
+        <Route path="/cart" element={<ShoppingCartPage />} /> {/* Route for the cart */}
       </Routes>
     </div>
   );
